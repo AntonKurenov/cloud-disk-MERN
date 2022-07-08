80cfd09e0b986e82d127747e568f689cb6d1df74
AntonKurenov <anton.kurenov@yandex.ru>
2022-07-08 23:31:16 +0300

    Added get files from server possibility to client

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,37 +1,44 @@
 import React, {useEffect} from "react";
 import Navbar from "./navbar/Navbar";
 import "./app.css"
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Registration from "./authorization/Registration";
 import Login from "./authorization/Login"
 import {useDispatch, useSelector} from "react-redux";
 import {setUser} from "../reducers/userReducer";
 import {auth} from "../actions/user";
+import Disk from "./disk/Disk";
 
 function App() {
-  const isAuth = useSelector(state => state.user.isAuth)
-  const dispatch = useDispatch()
+	const isAuth = useSelector(state => state.user.isAuth)
+	const dispatch = useDispatch()
 
-  useEffect(() => {
-    if (localStorage.getItem('token')) {
-      dispatch(auth())
-    }
-  }, [])
-  console.log("isAuth = ", isAuth)
+	useEffect(() => {
+		if (localStorage.getItem('token')) {
+			dispatch(auth())
+		}
+	}, [])
+	console.log("isAuth = ", isAuth)
 
-  return (
-    <BrowserRouter>
-      <div className="app">
-        <Navbar/>
-        {!isAuth &&
-          <Routes>
-            <Route path="/registration" element={<Registration/>}/>
-            <Route path="/login" element={<Login/>}/>
-          </Routes>
-        }
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className="app">
+				<Navbar/>
+				{!isAuth ?
+					<Routes>
+						<Route path="/registration" element={<Registration/>}/>
+						<Route path="/login" element={<Login/>}/>
+						<Route path="*" element={<Navigate to="/login"/>}/>
+					</Routes>
+					:
+					<Routes>
+						<Route path="/" element={<Disk/>}/>
+						<Route path="*" element={<Navigate to="/"/>}/>
+					</Routes>
+				}
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -31,7 +31,7 @@ class FileController {
 	async getFiles(req, res) {
 		try {
 			const files = await File.find({user: req.user.id, parent: req.query.parent})
-			return res.json({files})
+			return res.json(files)
 		} catch (e) {
 			console.log("catch error in getFiles = ", e)
 			return res.status(500).json({message: "Can't get files"})
